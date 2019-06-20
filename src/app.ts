7e2b976bc05788b63b06355f48ab2db322b7fdd9
Ruslan Gainutdinov <ruslan.gainutdinov@futurice.com>
2019-06-20 23:38:31 +0300

    Report commit status

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,69 @@
-import { Application } from 'probot'
+import { Application, Context } from 'probot'
 import { logger } from './logger'
 import { validateContent } from './validate'
 import { listGitFiles } from './git'
 
 import fs from 'fs'
+import { WebhookPayloadPullRequest } from '@octokit/webhooks'
+
+export const validate = async (context: Context<WebhookPayloadPullRequest>) => {
+  const pr = context.payload.pull_request
+  logger.info('Pull request edited', pr)
+
+  context.github.repos.createStatus({
+    context: 'Workflow-validator',
+    description: 'Checking',
+    repo: context.payload.repository.name,
+    owner: context.payload.repository.owner.login,
+    state: 'pending',
+    sha: pr.head.sha,
+  })
+
+  const ll = await listGitFiles(pr)
+  const bpmnFiles = ll.filter(f => f.endsWith('.bpmn'))
+  const check = await Promise.all(
+    bpmnFiles.map(
+      file =>
+        new Promise<{ file: string; success: boolean; result: string }>(async (resolve, reject) => {
+          await validateContent(fs.readFileSync(file).toString()).then(s => {
+            logger.info('Validation result', s)
+            resolve({
+              file,
+              success: !s.error,
+              result: s.error ? 'Failed' + s.error : 'Succeeded',
+            })
+          })
+        }),
+    ),
+  )
+
+  const errors = check.filter(f => !f.success).length
+
+  // JSON.stringify(ll, null, 2) + '\n\n' +
+  await context.github.issues
+    .createComment({
+      repo: context.payload.repository.name,
+      owner: context.payload.repository.owner.login,
+      issue_number: pr.number,
+      body: 'List of files ' + JSON.stringify(check, null, 2),
+      // body: 'Holly macaroni!\n\n```json\n' + JSON.stringify(context.payload, null, 2) + '\n```',
+    })
+    .then(res => {
+      logger.info('Create comment', res)
+    })
+    .catch(err => {
+      logger.warn('Failed to create comment', err)
+    })
+
+  context.github.repos.createStatus({
+    context: 'Workflow-validator',
+    description: errors > 0 ? 'Found errors in ' + errors + ' files' : 'Check done ok',
+    repo: context.payload.repository.name,
+    owner: context.payload.repository.owner.login,
+    state: errors > 0 ? 'failure' : 'success',
+    sha: pr.head.sha,
+  })
+}
 
 export const appHandler = (app: Application) => {
   logger.info('Created application handler')
@@ -16,37 +76,12 @@ export const appHandler = (app: Application) => {
     logger.info('Pull request opened', context.payload.pull_request)
   })
 
+  app.on('pull_request.synchronize', async context => {
+    await validate(context)
+  })
+
   app.on('pull_request.edited', async context => {
-    const pr = context.payload.pull_request
-    logger.info('Pull request edited', pr)
-    const ll = await listGitFiles(pr)
-    const bpmnFiles = ll.filter(f => f.endsWith('.bpmn'))
-    const check = await Promise.all(
-      bpmnFiles.map(
-        file =>
-          new Promise<{ file: string; result: string }>(async (resolve, reject) => {
-            await validateContent(fs.readFileSync(file).toString()).then(s => {
-              logger.info('Validation result', s)
-              resolve({ file, result: s.error ? 'Failed' + s.error : 'Succeeded' })
-            })
-          }),
-      ),
-    )
-    // JSON.stringify(ll, null, 2) + '\n\n' +
-    await context.github.issues
-      .createComment({
-        repo: context.payload.repository.name,
-        owner: context.payload.repository.owner.login,
-        issue_number: pr.number,
-        body: 'List of files ' + JSON.stringify(check, null, 2),
-        // body: 'Holly macaroni!\n\n```json\n' + JSON.stringify(context.payload, null, 2) + '\n```',
-      })
-      .then(res => {
-        logger.info('Create comment', res)
-      })
-      .catch(err => {
-        logger.warn('Failed to create comment', err)
-      })
+    await validate(context)
   })
 
   app.on('pull_request.labeled', async context => {
